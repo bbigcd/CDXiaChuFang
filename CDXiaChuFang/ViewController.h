accec05c7f99717c939df89df6641cc10ed3f958
bbigcd <bbigcd@163.com>
2017-08-16 23:05:37 +0800

    Initial Commit

---
+++ CDXiaChuFang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CDXiaChuFang
+//
+//  Created by chendi on 2017/8/16.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CDXiaChuFang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CDXiaChuFang
+//
+//  Created by chendi on 2017/8/16.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
