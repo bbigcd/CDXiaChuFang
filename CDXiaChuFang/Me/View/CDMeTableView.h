dce690d553ebe5bc3efac2900ae045c01bfe9099
bbigcd <bbigcd@163.com>
2017-09-08 22:02:08 +0800

    create me tableview file

---
+++ CDXiaChuFang/Me/View/CDMeTableView.h
@@ -0,0 +1,13 @@
+//
+//  CDMeTableView.h
+//  CDXiaChuFang
+//
+//  Created by chendi on 2017/9/8.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDMeTableView : UITableView
+
+@end
---
+++ CDXiaChuFang/Me/View/CDMeTableView.m
@@ -0,0 +1,21 @@
+//
+//  CDMeTableView.m
+//  CDXiaChuFang
+//
+//  Created by chendi on 2017/9/8.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import "CDMeTableView.h"
+
+@implementation CDMeTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
