1f218d996ff88dd52cd73766a965185ef195b27a
bbigcd <bbigcd@163.com>
2017-09-11 21:07:24 +0800

    created FavoriteVC

---
+++ CDXiaChuFang/Favorite/Controller/CDFavoriteViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDFavoriteViewController.h
+//  CDXiaChuFang
+//
+//  Created by chendi on 2017/9/11.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDFavoriteViewController : UIViewController
+
+@end
