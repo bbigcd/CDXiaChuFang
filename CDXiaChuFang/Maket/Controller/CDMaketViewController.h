9d9ef5cde8245b9f94f80fc5a660857f58ce8b68
bbigcd <bbigcd@163.com>
2017-09-12 21:33:24 +0800

    add MaketVC

---
+++ CDXiaChuFang/Maket/Controller/CDMaketViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDMaketViewController.h
+//  CDXiaChuFang
+//
+//  Created by chendi on 2017/9/12.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDMaketViewController : UIViewController
+
+@end
---
+++ CDXiaChuFang/Maket/Controller/CDMaketViewController.m
@@ -0,0 +1,49 @@
+//
+//  CDMaketViewController.m
+//  CDXiaChuFang
+//
+//  Created by chendi on 2017/9/12.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import "CDMaketViewController.h"
+
+@interface CDMaketViewController ()
+
+@end
+
+@implementation CDMaketViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc] init]forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setShadowImage:[[UIImage alloc] init]];
+    [self setupWebView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setupWebView
+{
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:webView];
+    NSURL *url = [NSURL URLWithString:@"https://www.xiachufang.com/page/ec-tab/"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:request];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
